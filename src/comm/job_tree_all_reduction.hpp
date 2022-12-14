fff4559ecec35754c82a597d0994a62956b170b8
Yu-An-Shih <ianshih1998@gmail.com>
2022-12-13 21:14:06 -0500

    local clause sharing

--- src/comm/job_tree_all_reduction.hpp
+++ src/comm/job_tree_all_reduction.hpp
@@ -36,6 +36,8 @@ private:
     bool _finished = false;
     bool _valid = true;
 
+    bool _sent_to_parent = false;
+
 public:
     JobTreeAllReduction(JobTree& jobTree, JobMessage baseMsg, AllReduceElement&& neutralElem, 
             std::function<AllReduceElement(std::list<AllReduceElement>&)> aggregator) :
@@ -63,6 +65,41 @@ public:
 
     // Process an incoming message and advance the all-reduction accordingly. 
     bool receive(int source, int tag, JobMessage& msg) {
+        
+        assert(tag == MSG_JOB_TREE_REDUCTION || tag == MSG_JOB_TREE_BROADCAST);
+
+        bool accept = msg.jobId == _base_msg.jobId 
+                    && msg.epoch == _base_msg.epoch 
+                    && msg.revision == _base_msg.revision 
+                    && msg.tag == _base_msg.tag;
+        if (!accept) return false;
+
+        if (tag == MSG_JOB_TREE_REDUCTION) {
+            
+            if (_aggregating || _future_aggregate.valid() || _reduction_locally_done) 
+                return false; // already internally aggregating elements (or already done)!
+            
+            // check if this message comes from a child which didn't already send something
+            bool fromLeftChild = !_received_child_elems.first && source == _expected_child_ranks.first;
+            bool fromRightChild = !_received_child_elems.second && source == _expected_child_ranks.second;
+            accept &= fromLeftChild || fromRightChild;
+            if (!accept) return false;
+            
+            // message accepted: store and check off
+            _child_elems.push_back(std::move(msg.payload));
+            if (fromLeftChild) _received_child_elems.first = true;
+            if (fromRightChild) _received_child_elems.second = true;
+            LOG_ADD_SRC(V5_DEBG, "CS got %i/%i elems", source, _child_elems.size(), _num_expected_child_elems);
+            advance();
+        }
+        if (tag == MSG_JOB_TREE_BROADCAST) {
+            _finished = true;
+            _base_msg.payload = std::move(msg.payload);
+        }
+        return true;
+    }
+    
+    bool receive_origin(int source, int tag, JobMessage& msg) {
 
         assert(tag == MSG_JOB_TREE_REDUCTION || tag == MSG_JOB_TREE_BROADCAST);
 
@@ -99,6 +136,51 @@ public:
     // Advances the all-reduction, e.g., because the local producer finished
     // or the aggregation function finished. No-op if getResult() was already called.
     void advance() {
+        if (_finished) return;
+
+        // Send local produced clauses/filter to parent node
+        if (!_tree.isRoot() && !_sent_to_parent && _local_elem.has_value()) {
+            _base_msg.payload = _local_elem.value();
+            MyMpi::isend(_tree.getParentNodeRank(), MSG_JOB_TREE_REDUCTION, _base_msg);
+            _sent_to_parent = true;
+        }
+
+        // Aggregate clauses/filter
+        if (!_tree.isLeaf() && _child_elems.size() == _num_expected_child_elems && _local_elem.has_value()) {
+            _child_elems.push_front(std::move(_local_elem.value()));
+            _local_elem.reset();
+
+            assert(!_future_aggregate.valid());
+            _aggregating = true;
+            _future_aggregate = ProcessWideThreadPool::get().addTask([&]() {
+                _aggregated_elem = _aggregator(_child_elems);
+                _aggregating = false;
+            });
+        }
+        
+        if (!_aggregating && _future_aggregate.valid()) {
+            // Aggregation done
+            _future_aggregate.get();
+            _reduction_locally_done = true;
+            
+            if (_tree.isRoot()) {
+                // Transform reduced element at root  /* Q: what is this for? */
+                if (_has_transformation_at_root) {
+                    _aggregated_elem.emplace(_transformation_at_root(_aggregated_elem.value()));
+                }
+                _finished = true;
+            }
+
+            _base_msg.payload = std::move(_aggregated_elem.value());
+
+            if (_expected_child_ranks.first >= 0) 
+                MyMpi::isend(_expected_child_ranks.first, MSG_JOB_TREE_BROADCAST, _base_msg);
+            if (_expected_child_ranks.second >= 0) 
+                MyMpi::isend(_expected_child_ranks.second, MSG_JOB_TREE_BROADCAST, _base_msg);
+        }
+    }
+
+    void advance_origin() {
 
         if (_finished) return;
 
