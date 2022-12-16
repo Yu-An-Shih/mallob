736dc06d70b86735afa3d0df6fd630f3d3c39417
Yu-An-Shih <ianshih1998@gmail.com>
2022-12-15 22:06:53 -0500

    add executables and results

--- mallob-exe/local_clause_sharing/run_case.sh
+++ mallob-exe/local_clause_sharing/run_case.sh
@@ -14,18 +14,18 @@ sat_case_3="${case_dir}/size_5_5_5_i053_r12.cnf.xz"
 unsat_case_1="${case_dir}/crafted_n11_d6_c4_num19.cnf.xz"
 unsat_case_3="${case_dir}/w16-5.cnf.xz"
 
-#I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=2 --ntasks=32 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -mono=$sat_case_1 -satsolver=l -jwl=500 -v=0 2>&1 > sat_case_1-32.txt
-#I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=2 --ntasks=32 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -mono=$sat_case_3 -satsolver=l -jwl=500 -v=0 2>&1 > sat_case_3-32.txt
-#I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=2 --ntasks=32 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -mono=$unsat_case_1 -satsolver=l -jwl=500 -v=0 2>&1 > unsat_case_1-32.txt
-I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=2 --ntasks=32 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -mono=$unsat_case_3 -satsolver=l -jwl=1000 -v=0 2>&1 > unsat_case_3-32.txt
+I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=3 --ntasks=48 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -s=0.4 -cbbs=800 -mono=$sat_case_1 -satsolver=l -jwl=500 -v=0 2>&1 > sat_case_1-48.txt
+I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=3 --ntasks=48 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -s=0.4 -cbbs=800 -mono=$sat_case_3 -satsolver=l -jwl=500 -v=0 2>&1 > sat_case_3-48.txt
+I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=3 --ntasks=48 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -s=0.4 -cbbs=800 -mono=$unsat_case_1 -satsolver=l -jwl=500 -v=0 2>&1 > unsat_case_1-48.txt
+I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=3 --ntasks=48 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -s=0.4 -cbbs=800 -mono=$unsat_case_3 -satsolver=l -jwl=1000 -v=0 2>&1 > unsat_case_3-48.txt
 
-#I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=3 --ntasks=48 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -mono=$sat_case_1 -satsolver=l -jwl=500 -v=0 2>&1 > sat_case_1-48.txt
-#I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=3 --ntasks=48 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -mono=$sat_case_3 -satsolver=l -jwl=500 -v=0 2>&1 > sat_case_3-48.txt
-#I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=3 --ntasks=48 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -mono=$unsat_case_1 -satsolver=l -jwl=500 -v=0 2>&1 > unsat_case_1-48.txt
-I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=3 --ntasks=48 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -mono=$unsat_case_3 -satsolver=l -jwl=1000 -v=0 2>&1 > unsat_case_3-48.txt
-
-#sbatch run_case_16.slurm
-#sbatch run_case_8.slurm
-#sbatch run_case_4.slurm
-#sbatch run_case_2.slurm
+sbatch run_case_16.slurm
+sbatch run_case_8.slurm
+sbatch run_case_4.slurm
+sbatch run_case_2.slurm
 #sbatch run_case_1.slurm
+
+I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=2 --ntasks=32 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -s=0.4 -cbbs=800 -mono=$sat_case_1 -satsolver=l -jwl=500 -v=0 2>&1 > sat_case_1-32.txt
+I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=2 --ntasks=32 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -s=0.4 -cbbs=800 -mono=$sat_case_3 -satsolver=l -jwl=500 -v=0 2>&1 > sat_case_3-32.txt
+I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=2 --ntasks=32 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -s=0.4 -cbbs=800 -mono=$unsat_case_1 -satsolver=l -jwl=500 -v=0 2>&1 > unsat_case_1-32.txt
+I_MPI_OFI_PROVIDER=tcp srun --partition=malik --nodes=2 --ntasks=32 --cpus-per-task=4 --time=00:10:00 build/mallob -t=4 -s=0.4 -cbbs=800 -mono=$unsat_case_3 -satsolver=l -jwl=1000 -v=0 2>&1 > unsat_case_3-32.txt
--- mallob-exe/plot.py
+++ mallob-exe/plot.py
@@ -1,14 +1,19 @@
 import matplotlib.pyplot as plt
+from functools import partial
+import numpy as np
+
 import re
 
-def fetch_values(directory, cases, num_pe, total_times, comm_mum_epochs, comm_avg_times):
+import math
+
+def fetch_values(directory, cases, num_pe, total_times, comm_num_epochs, comm_avg_times):
     
-    comm_num_epochs_debug = [0 for i in range(len(comm_mum_epochs))]
+    comm_num_epochs_debug = [0 for i in range(len(comm_num_epochs))]
 
     for i in range(len(num_pe)):
         for case in cases:
             found_result = False
-            comm_num_epochs_debug[i] = comm_mum_epochs[i]
+            comm_num_epochs_debug[i] = comm_num_epochs[i]
             
             try:
                 f = open(directory + case + "-" + str(num_pe[i]) + ".txt", "r")
@@ -29,9 +34,9 @@ def fetch_values(directory, cases, num_pe, total_times, comm_mum_epochs, comm_av
                 
                 if re.search("Communication", line) != None:
                     digits = re.findall("\s\d+\s", line)
-                    assert len(digits) == 2                    
-                    assert int(digits[1]) == comm_mum_epochs[i] + 1 - comm_num_epochs_debug[i]
-                    comm_mum_epochs[i] += 1
+                    assert len(digits) == 2
+                    assert int(digits[1]) == comm_num_epochs[i] + 1 - comm_num_epochs_debug[i]
+                    comm_num_epochs[i] += 1
                     
                     digits = re.findall("\d+\.\d+", line)
                     assert len(digits) == 2
@@ -46,19 +51,22 @@ def fetch_values(directory, cases, num_pe, total_times, comm_mum_epochs, comm_av
             
             f.close()
     
-    total_times = [x / len(cases) for x in total_times]
+    for i in range(len(total_times)):
+        total_times[i] /= len(cases)
     print(total_times)
 
-    for i in range(len(comm_mum_epochs)):
-        if comm_mum_epochs[i] != 0:
-            comm_avg_times[i] /= comm_mum_epochs[i]
-    print(comm_mum_epochs)
+    for i in range(len(comm_num_epochs)):
+        if comm_num_epochs[i] != 0:
+            comm_avg_times[i] /= comm_num_epochs[i]
+            comm_num_epochs[i] /= len(cases)
+    print(comm_num_epochs)
     print(comm_avg_times)
 
 
 def main():
     old_dir = "original/results/"
     new_dir = "local_clause_sharing/results/"
+    new_fast_dir = "local_clause_sharing/results-fast_share/"
     
     cases = ["sat_case_1", "sat_case_3", "unsat_case_1", "unsat_case_3"]
 
@@ -66,31 +74,37 @@ def main():
     
     old_total_times = [0, 0, 0, 0, 0, 0, 0]
     new_total_times = [0, 0, 0, 0, 0, 0, 0]
+    new_fast_total_times = [0, 0, 0, 0, 0, 0, 0]
     
-    old_comm_mum_epochs = [0, 0, 0, 0, 0, 0, 0]
+    old_comm_num_epochs = [0, 0, 0, 0, 0, 0, 0]
     old_comm_avg_times = [0, 0, 0, 0, 0, 0, 0]
-    new_comm_mum_epochs = [0, 0, 0, 0, 0, 0, 0]
+    new_comm_num_epochs = [0, 0, 0, 0, 0, 0, 0]
     new_comm_avg_times = [0, 0, 0, 0, 0, 0, 0]
+    new_fast_comm_num_epochs = [0, 0, 0, 0, 0, 0, 0]
+    new_fast_comm_avg_times = [0, 0, 0, 0, 0, 0, 0]
     
-    fetch_values(old_dir, cases, num_pe, old_total_times, old_comm_mum_epochs, old_comm_avg_times)
-    fetch_values(new_dir, cases, num_pe, new_total_times, new_comm_mum_epochs, new_comm_avg_times)
+    fetch_values(old_dir, cases, num_pe, old_total_times, old_comm_num_epochs, old_comm_avg_times)
+    fetch_values(new_dir, cases, num_pe, new_total_times, new_comm_num_epochs, new_comm_avg_times)
+    fetch_values(new_fast_dir, cases, num_pe, new_fast_total_times, new_fast_comm_num_epochs, new_fast_comm_avg_times)
 
 
     # plot
-    x_index = list(range(len(num_pe)))
-    plt.xticks(x_index, num_pe)
-
-    #plt.title('')
-
-    plt.xlabel('# PEs')
-    plt.ylabel('avg. runtime (s)')
-
-    plt.plot(x_index, old_total_times, marker = 'o', linestyle = '--', label = 'original approach')
-    plt.plot(x_index, new_total_times, marker = 'o', linestyle = '--', label = 'local clause sharing')
-    plt.legend()
+    fig, ax = plt.subplots()
     
-    plt.savefig('timing.png')
-    plt.close()
+    x_index = [0, 1, 2, 3, 4, 5, math.log2(48)]
+    ax.set_xticks(x_index)
+    ax.set_xticklabels(num_pe)
+
+    ax.set_xlabel('# PEs')
+    ax.set_ylabel('avg. runtime (s)')
+
+    ax.plot(x_index, old_total_times, marker = 'o', linestyle = '--', label = 'original approach')
+    ax.plot(x_index[1:], new_total_times[1:], marker = '^', linestyle = '--', label = 'local clause sharing')
+    ax.plot(x_index[1:], new_fast_total_times[1:], marker = 's', linestyle = '--', label = 'local clause sharing w/ fast sharing')
+    ax.legend()
+
+    fig.savefig('timing.png')
+    plt.close(fig)
 
     
     #fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True)
@@ -105,16 +119,18 @@ def main():
     ax2.set_xticklabels(num_pe[1:])
     
     ax1.set_xlabel('# PEs')
-    ax1.set_ylabel('# communication sessions')
+    ax1.set_ylabel('avg. # communication sessions')
     ax2.set_xlabel('# PEs')
     ax2.set_ylabel('avg. session time (s)')
 
-    ax1.plot(x_index[1:], old_comm_mum_epochs[1:], marker = 'o', linestyle = '--', label = 'original approach')
-    ax1.plot(x_index[1:], new_comm_mum_epochs[1:], marker = 'o', linestyle = '--', label = 'local clause sharing')
+    ax1.plot(x_index[1:], old_comm_num_epochs[1:], marker = 'o', linestyle = '--', label = 'original approach')
+    ax1.plot(x_index[1:], new_comm_num_epochs[1:], marker = '^', linestyle = '--', label = 'local clause sharing')
+    ax1.plot(x_index[1:], new_fast_comm_num_epochs[1:], marker = 's', linestyle = '--', label = 'local clause sharing w/ fast sharing')
     ax1.legend()
 
     ax2.plot(x_index[1:], old_comm_avg_times[1:], marker = 'o', linestyle = '--', label = 'original approach')
-    ax2.plot(x_index[1:], new_comm_avg_times[1:], marker = 'o', linestyle = '--', label = 'local clause sharing')
+    ax2.plot(x_index[1:], new_comm_avg_times[1:], marker = '^', linestyle = '--', label = 'local clause sharing')
+    ax2.plot(x_index[1:], new_fast_comm_avg_times[1:], marker = 's', linestyle = '--', label = 'local clause sharing w/ fast sharing')
     ax2.legend()
 
     fig.savefig('comm.png')
